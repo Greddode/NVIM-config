d09da6717943c486bfc0b7ef603ec2c4d947cd06
Greddode Ashenforge <patrickrich07@gmail.com>
2025-11-02 12:20:38 -0600

    Updated to 0.11 nvim LSPs and added Javascript LSP and Blink

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,21 +9,20 @@ return {
     "williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "rust-analyzer" }
+				ensure_installed = { "lua_ls", "ts_ls", "rust_analyzer" }
 			})
 		end
   },
   {
     "neovim/nvim-lspconfig",
     	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({})
-		lspconfig.tsserver.setup({})
-		lspconfig.rustanalyzer.setup({})
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("ts_ls")
+			vim.lsp.enable("rust_analyzer")
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+	      		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
 }
